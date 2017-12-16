df2c66246783e9f44954022018d0b294da7564ab
Christopher Dambamuromo <chridam@Christophers-MacBook-Pro.local>
2017-12-16 22:21:29 +0000

    Update books using API

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,23 +1,23 @@
 import React from 'react';
 
-const Book = ({ id, title, author, imageUrl, onMove=f=>f }) => {
+const Book = ({ title, authors, imageLinks, shelf, onMove=f=>f }) => {
     return (
         <li>
             <div className="book">
             <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${imageUrl}")` }}></div>
+                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${imageLinks.thumbnail}")` }}></div>
                 <div className="book-shelf-changer">
-                <select onChange={(e)=> {onMove(id, e.target.value)}}>
+                <select onChange={(e)=> {onMove(e.target.value)}}>
                     <option value="none" disabled>Move to...</option>
                     <option value="currentlyReading">Currently Reading</option>
                     <option value="wantToRead">Want to Read</option>
                     <option value="read">Read</option>
-                    <option value="none">None</option>
+                    <option value={shelf}>None</option>
                 </select>
                 </div>
             </div>
             <div className="book-title">{title}</div>
-            <div className="book-authors">{author}</div>
+            <div className="book-authors">{authors.join(', ')}</div>
             </div>
         </li>
     )
--- src/components/ListReads.js
+++ src/components/ListReads.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom'
 import BookShelf from './BookShelf';
 
 const ListReads = ({ books, onMove=f=>f }) => {
@@ -15,7 +16,7 @@ const ListReads = ({ books, onMove=f=>f }) => {
         </div>
       </div>
       <div className="open-search">
-        <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+        <Link to='/search'>Add a book</Link>
       </div>
     </div>
   )
